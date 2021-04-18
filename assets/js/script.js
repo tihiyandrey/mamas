78d5dafe54e2eb33ee857c3b1aef66cdb283fe1a
Tikhiy Andrey <a.tikhiy@vipcoingold.com>
2021-04-18 23:04:04 +0300

    Add login pages.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,43 +7,63 @@ const specialistsPopUp = document.getElementsByClassName('specialists-pop-up'),
       popUpRemind = document.querySelectorAll('.pop-up-remind'),
       openRemind = document.querySelectorAll('.open-remind');
 
-      blockMobileOpen.forEach((element, i) => {
-        element.addEventListener('click', () => {
-            if (window.screen.width >= 768){
-                specialistsPopUp[0].classList.add('pop-up_active');
-                blackout.classList.add('blackout_active');
-            } else{
-                btnMobileOpen[i].classList.toggle('specialists__mobile-open-btn_active');
-                element.classList.toggle('specialists__item_mobile-open');
-            }
-          });
-      });
-      popUpClose.forEach((element, i) => {
-          element.addEventListener('click', () =>{
-            blackout.classList.remove('blackout_active');
-            popUp[i].classList.remove('pop-up_active');
-          })
-      });
-      openRemind.forEach((element, i) => {
-        element.addEventListener('click', () => {
-            popUpRemind[0].classList.add("pop-up_active");
+blockMobileOpen.forEach((element, i) => {
+    element.addEventListener('click', () => {
+        if (window.screen.width >= 768){
+            specialistsPopUp[0].classList.add('pop-up_active');
             blackout.classList.add('blackout_active');
-        })
-      });
-    
-    $(document).ready(function(){
-      $(".owl-carousel").owlCarousel({
-        center: true,
-        items:1,
-        loop:false,
-        margin:10,
-        responsive:{
-            800:{
-                items:3
-            },
-            320:{
-              items:1
-          }
+        } else{
+            btnMobileOpen[i].classList.toggle('specialists__mobile-open-btn_active');
+            element.classList.toggle('specialists__item_mobile-open');
         }
-      });
     });
+});
+popUpClose.forEach((element, i) => {
+    element.addEventListener('click', () =>{
+        blackout.classList.remove('blackout_active');
+        popUp[i].classList.remove('pop-up_active');
+    })
+});
+openRemind.forEach((element, i) => {
+    element.addEventListener('click', () => {
+        popUpRemind[0].classList.add("pop-up_active");
+        blackout.classList.add('blackout_active');
+    })
+});
+
+const titleName = $('.advantages__first-item').html();
+function owlInitialize() {
+    if ($(window).width() < 769) {
+        $('.advantages__first-item').remove();
+        $('.advantages__wrap').addClass("owl-carousel");
+        $('.coming-soon__wrap').addClass("owl-carousel");
+        $('.owl-carousel').owlCarousel({
+            center: true,
+            loop:true,
+            margin:10,
+            stagePadding: 40,
+            dots: true,
+            responsive:{
+                600:{
+                    items:3
+                },
+                320:{
+                    items:1
+                }
+            }
+        });
+    }else{
+        $('.owl-carousel').owlCarousel('destroy');
+        $('.advantages__wrap').removeClass("owl-carousel");
+        $('.coming-soon__wrap').removeClass("owl-carousel");
+        if($('.advantages__first-item').length === 0) {
+            $('.advantages__wrap').prepend('<div class="advantages__first-item">' + titleName + '</div>');
+        }
+    }
+}
+$(document).ready(function(e) {
+    owlInitialize();
+});
+$(window).resize(function() {
+    owlInitialize();
+});
